e5831ba11f6ee1030cc379064bc7870e8b648f71
Tanvir <tanvir.ahmeed.aiub@gmail.com>
2022-05-30 13:50:36 +0600

    Intro MVC

---
+++ IntroMVC/IntroMVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Login() {
+            ViewBag.University = "AIUB";
+            return View();
+        }
+        public ActionResult Dashboard() {
+            ViewBag.Name = "Tanvir Ahmed";
+            ViewBag.Id = "12121";
+             
+            //ViewData["Fname"] = "Tanvir";
+           return View();
+        }
+        public ActionResult About() {
+            //return Redirect("/home/dashboard");
+            TempData["Msg"] = "Redirected from About";
+            return RedirectToAction("Login");  //to redirect /home/login
+            //return RedirectToAction("Dashboard", "Profie"); //to redirect /profile/dashboard
+
+        }
+    }
+}
---
+++ IntroMVC/IntroMVC/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using IntroMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < 10; i++) {
+                /*
+                 * Student s = new Student();
+                 * s.Id = i+1;
+                 * s.Name = "Student " + (i+1),
+                 * s.Dob = "";
+                 * students.Add(s);
+                 */
+                students.Add(new Student()
+                {
+                    Id = i+1,
+                    Name = "Student " + (i+1),
+                    Dob = "11-11-11"
+                });
+            }
+            return View(students);
+        }
+        public ActionResult Info() {
+            Student s1 = new Student() { 
+                Id = 1,
+                Name = "Tanvir",
+                Dob = "111-111-1"
+            };
+            return View(s1);
+        }
+    }
+}
